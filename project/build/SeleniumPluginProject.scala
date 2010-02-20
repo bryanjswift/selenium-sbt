8b91916b41ba5015d3d941592b4fd77e2ef0096c
Bryan J Swift <bryan.j.swift@gmail.com>
2010-02-20 17:30:17 -0500

    Add additional dependencies
    They are required because of hamcrest-all-1.1.jar is expanded as a
    source jar and then compiled.

--- project/build/SeleniumPluginProject.scala
+++ project/build/SeleniumPluginProject.scala
@@ -3,6 +3,9 @@ import sbt._
 
 class SeleniumPluginProject(info:ProjectInfo) extends PluginProject(info:ProjectInfo) with ResolverPlugin {
 	val seleniumServer = "org.seleniumhq.selenium" % "selenium-server" % "2.0a2"
+	val qdox = "com.thoughtworks.qdox" % "qdox" % "1.9.2"
+	val jmock = "jmock" % "jmock" % "1.2.0"
+	val easymock = "org.easymock" % "easymock" % "2.5.2"
 
 	// publish to a Maven style repository
 	override def managedStyle = ManagedStyle.Maven
