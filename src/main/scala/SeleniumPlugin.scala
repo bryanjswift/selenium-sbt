18413cdaadeb3c5bb592ac2ecf1b9773f9b21826
Bryan Swift <bryans@arc90.com>
2010-02-25 13:53:04 -0500

    Change task structure inside SeleniumPlugin
    Tasks are now (again?) created from private methods. Create a new
    seleniumTest task which handles starting up the server as well as
    shutting down the server in the cleanup. Bump version.

--- src/main/scala/SeleniumPlugin.scala
+++ src/main/scala/SeleniumPlugin.scala
@@ -1,37 +1,42 @@
-package selenium
+package bryanjswift.selenium
 
 import org.openqa.selenium.server.{RemoteControlConfiguration,SeleniumServer}
 import sbt._
 
 trait SeleniumPlugin {
-	self: Project =>
-	val seleniumConfig = new RemoteControlConfiguration()
-	private var server:Option[SeleniumServer] = None
+	self: BasicScalaProject =>
+	private val seleniumConfig = new RemoteControlConfiguration()
+	private val server = new SeleniumServer(seleniumConfig)
+	private val seleniumTestOptions = testOptions ++ List(TestCleanup(stopServer _))
 
-	lazy val seleniumStart = task {
+	private def startServer =
 		try {
-			if (server == None) {
-				server = Some(new SeleniumServer(seleniumConfig))
-			} else {
-				server.get.stop
-				server.get.start
-			}
+			server.stop
+			server.start
 			None
 		} catch {
+			case mex:org.openqa.jetty.util.MultiException =>
+				/* this is thrown even if the server starts up correctly, not sure why
+					MultiException usually consists of a BindException (port in use) and
+					a class not found exception (org.openqa.selenium.remote.server.DriverServlet) */
+				None
 			case e =>
-				e.printStackTrace
 				Some("Could not start Selenium Server because of: " + e.getMessage)
 		}
-	}
+	lazy val seleniumStart = task { startServer }
 
-	lazy val seleniumStop = task {
+	private def stopServer =
 		try {
-			if (server.isDefined) { server.get.stop }
+			server.stop
 			None
 		} catch {
-			case e =>
-				Some("Could not stop Selenium Server because of: " + e.getMessage)
+			case e => Some("Could not stop Selenium Server because of: " + e.getMessage)
 		}
-	}
+	lazy val seleniumStop = task { stopServer }
+
+	lazy val seleniumTest =
+		testTask(testFrameworks, testClasspath, testCompileConditional.analysis, seleniumTestOptions)
+			.dependsOn(testCompile, copyResources, copyTestResources, seleniumStart) describedAs BasicScalaProject.TestDescription
+
 }
 
